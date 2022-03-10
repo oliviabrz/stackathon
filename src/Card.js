4d4c628c8e959c5a4a9534e5411e013dd1f6120e
Olivia <oliviabrz@gmail.com>
2022-03-10 16:30:50 -0600

    added background color, button and gif

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import './App.css';
+import pond from "./pond.gif"
 
-function Card() {
-    return <div>Hello!</div>;
+export default class Card extends React.Component {
+  playAudio() {
+    const audioEl = document.getElementsByClassName('audio-element')[0];
+    audioEl.play();
+  }
+  render() {
+    return (
+      <div>
+        <h1>Happy Birthday Mom!</h1>
+        <img src={pond} alt='wait until the page loads'/>
+        <div className='Button'>
+          <button onClick={this.playAudio}>
+            <span>Play Audio</span>
+          </button>
+          <audio>
+            <source src='Happy-birthday-piano-music.mov'></source>
+          </audio>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Card
+// export default class Card extends React.Component {
+//   componentDidMount() {
+//     const audioEl = document.getElementsByClassName("audio-element")[0]
+//     audioEl.play()
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//           <div className='Card'></div>
+//         <audio className="audio-element">
+//           <source src="Happy-birthday-piano-music.mov"></source>
+//         </audio>
+//       </div>
+//     )
+//   }
+// }
